62440d45f13a1689bd4d7679783c0a70b22b6a05
baosi <baosi@sm.vvip-u.com>
2018-04-17 17:57:14 +0800

    change the xg2

--- client/app/bsDemo/tpls/home/home.Controller.js
+++ client/app/bsDemo/tpls/home/home.Controller.js
@@ -195,7 +195,7 @@ angular.module('shenmaApp')
             [index]: number,
         }))
 
-        class Queue3 {
+        class Queue2 {
             constructor(contents = []) {
                 this.queue = [...contents];
                 this.number = 23;
